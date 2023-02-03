6a9a42c7f116072001dabde13f2857ddde282b18
Sean McGuire <seanmcgu@andrew.cmu.edu>
2023-02-03 11:18:28 -0500

    Add more pixel node unit tests

--- tests/hipscat/catalog/test_pixel_node.py
+++ tests/hipscat/catalog/test_pixel_node.py
@@ -10,7 +10,7 @@ def check_node_data_correct(node, node_dict):
     assert node.parent == node_dict["parent"]
 
 
-def test_create_root_node_correct(root_pixel_node_data):
+def test_create_root_node(root_pixel_node_data):
     root_node = PixelNode(**root_pixel_node_data)
     assert root_node
     check_node_data_correct(root_node, root_pixel_node_data)
@@ -22,9 +22,55 @@ def test_create_root_node_with_parent_raises_error(root_pixel_node, root_pixel_n
         PixelNode(**root_pixel_node_data)
 
 
-def test_create_leaf_node(leaf_pixel_node_data, root_pixel_node):
+def test_create_inner_node(inner_pixel_node_data, root_pixel_node):
+    leaf_node = PixelNode(**inner_pixel_node_data)
+    assert leaf_node
+    check_node_data_correct(leaf_node, inner_pixel_node_data)
+    assert len(root_pixel_node.children) == 1
+    assert leaf_node in root_pixel_node.children
+
+
+def test_create_inner_node_with_no_parent_raises_error(inner_pixel_node_data):
+    with pytest.raises(ValueError):
+        inner_pixel_node_data["parent"] = None
+        PixelNode(**inner_pixel_node_data)
+
+
+def test_create_leaf_node(leaf_pixel_node_data, inner_pixel_node):
     leaf_node = PixelNode(**leaf_pixel_node_data)
     assert leaf_node
     check_node_data_correct(leaf_node, leaf_pixel_node_data)
-    assert len(root_pixel_node.children) == 1
-    assert leaf_node in root_pixel_node.children
+    assert len(inner_pixel_node.children) == 1
+    assert leaf_node in inner_pixel_node.children
+
+
+def test_create_leaf_node_with_no_parent_raises_error(leaf_pixel_node_data):
+    with pytest.raises(ValueError):
+        leaf_pixel_node_data["parent"] = None
+        PixelNode(**leaf_pixel_node_data)
+
+
+def test_create_leaf_node_with_no_hp_order_raises_error(leaf_pixel_node_data):
+    with pytest.raises(ValueError):
+        leaf_pixel_node_data["hp_order"] = None
+        PixelNode(**leaf_pixel_node_data)
+
+
+def test_create_leaf_node_with_no_hp_pixel_raises_error(leaf_pixel_node_data):
+    with pytest.raises(ValueError):
+        leaf_pixel_node_data["hp_pixel"] = None
+        PixelNode(**leaf_pixel_node_data)
+
+
+def test_get_leaf_descendants_with_leaf_node(leaf_pixel_node):
+    assert leaf_pixel_node.get_all_leaf_descendants() == [leaf_pixel_node]
+
+
+def test_get_leaf_descendants_with_root_node(root_pixel_node, leaf_pixel_node):
+    assert root_pixel_node.get_all_leaf_descendants() == [leaf_pixel_node]
+
+
+def test_get_leaf_descendants_with_multiple_leafs(root_pixel_node, leaf_pixel_node, leaf_pixel_node_data):
+    leaf_pixel_node_data["hp_pixel"] += 1
+    second_leaf = PixelNode(**leaf_pixel_node_data)
+    assert root_pixel_node.get_all_leaf_descendants() == [leaf_pixel_node, second_leaf]
