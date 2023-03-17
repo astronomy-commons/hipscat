1eeefb7e5078c92096d92e2391958040c64f8f92
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-03-17 08:48:05 -0400

    Oopsies

--- tests/hipscat/pixel_math/test_hipscat_id.py
+++ tests/hipscat/pixel_math/test_hipscat_id.py
@@ -3,7 +3,9 @@
 import numpy as np
 import numpy.testing as npt
 import pytest
-from hipscat_import.hipscat_id import compute_hipscat_id, hipscat_id_to_healpix
+
+from hipscat.pixel_math.hipscat_id import (compute_hipscat_id,
+                                           hipscat_id_to_healpix)
 
 
 def test_single():
