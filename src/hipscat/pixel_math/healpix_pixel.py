73dc6b5d9a804dff45bb7079352495f0dca5b1d0
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-04-06 15:32:58 -0400

    Let dataclass do more lifting.

--- src/hipscat/pixel_math/healpix_pixel.py
+++ src/hipscat/pixel_math/healpix_pixel.py
@@ -7,7 +7,7 @@ import hipscat.pixel_math.hipscat_id
 MAXIMUM_ORDER = hipscat.pixel_math.hipscat_id.HIPSCAT_ID_HEALPIX_ORDER
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 class HealpixPixel:
     """A HEALPix pixel, represented by an order and pixel number in NESTED ordering scheme
 
@@ -25,21 +25,6 @@ class HealpixPixel:
         """
         if self.order > MAXIMUM_ORDER:
             raise ValueError(f"HEALPix order cannot be greater than {MAXIMUM_ORDER}")
-
-    def _key(self) -> tuple[int, int]:
-        """Returns tuple of order and pixel, for use in hashing and equality"""
-        return self.order, self.pixel
-
-    def __eq__(self, other: object) -> bool:
-        """Defines 2 pixels as equal if they have the same order and pixel"""
-        if not isinstance(other, HealpixPixel):
-            return False
-        return self._key() == other._key()
-
-    def __hash__(self) -> int:
-        """Hashes pixels by order and pixel, so equal pixel objects are looked up the same in
-        hashable data structures"""
-        return hash(self._key())
 
     def __str__(self) -> str:
         return f"Order: {self.order}, Pixel: {self.pixel}"
