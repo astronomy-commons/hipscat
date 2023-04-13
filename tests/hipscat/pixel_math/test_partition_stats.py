3fe440b739621d064f0656b94ee3717bf304b7ea
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-04-13 09:37:37 -0400

    Check error messages in tests.

--- tests/hipscat/pixel_math/test_partition_stats.py
+++ tests/hipscat/pixel_math/test_partition_stats.py
@@ -38,11 +38,12 @@ def test_column_names_error():
         columns=["id", "ra_mean", "dec_mean"],
     )
 
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as error:
         hist.generate_histogram(
             data=data,
             highest_order=0,
         )
+        assert "Invalid column names" in error.value
 
 
 def test_column_names():
@@ -69,15 +70,17 @@ def test_column_names():
 def test_alignment_wrong_size():
     """Check that the method raises error when the input histogram is not the expected size."""
     initial_histogram = np.asarray([0, 0, 0, 0, 0, 0, 0, 0, 0, 131])
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as error:
         hist.generate_alignment(initial_histogram, 0, 250)
+        assert "histogram is not the right size" == error.value
 
 
 def test_alignment_exceeds_threshold_order0():
     """Check that the method raises error when some pixel exceeds the threshold."""
     initial_histogram = np.asarray([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 131])
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as error:
         hist.generate_alignment(initial_histogram, 0, 20)
+        assert "exceeds threshold" in error.value
 
 
 def test_alignment_exceeds_threshold_order2():
@@ -85,8 +88,9 @@ def test_alignment_exceeds_threshold_order2():
     initial_histogram = hist.empty_histogram(2)
     filled_pixels = [4, 11, 14, 13, 5, 7, 8, 9, 11, 23, 4, 4, 17, 0, 1, 0]
     initial_histogram[176:] = filled_pixels[:]
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as error:
         hist.generate_alignment(initial_histogram, 2, 20)
+        assert "exceeds threshold" in error.value
 
 
 def test_alignment_small_sky_order0():
@@ -193,6 +197,7 @@ def test_compute_pixel_map_order1():
 
     npt.assert_array_equal(result, expected)
 
+
 def test_compute_pixel_map_invalid_inputs():
     """Create destination pixel map for small sky at order 1"""
 
@@ -201,9 +206,11 @@ def test_compute_pixel_map_invalid_inputs():
     initial_histogram[44:] = filled_pixels[:]
 
     ## Order doesn't match histogram length
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as length_error:
         hist.compute_pixel_map(initial_histogram, highest_order=2, threshold=150)
+        assert "histogram is not the right size" in length_error.value
 
     ## Some bins exceed threshold
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as threshold_error:
         hist.compute_pixel_map(initial_histogram, highest_order=1, threshold=30)
+        assert "exceeds threshold" in threshold_error.value
