579fa1792fd745a689e9fb8d928a28df88aeab51
Sean McGuire <seanmcgu@andrew.cmu.edu>
2023-04-27 12:31:10 -0400

    add pixel alignment tree function

--- src/hipscat/pixel_math/healpix_pixel.py
+++ src/hipscat/pixel_math/healpix_pixel.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import math
 from dataclasses import dataclass
+from typing import List
 
 from hipscat.pixel_math.hipscat_id import HIPSCAT_ID_HEALPIX_ORDER
 
@@ -31,3 +33,21 @@ class HealpixPixel:
 
     def __repr__(self):
         return self.__str__()
+
+    def convert_to_lower_order(self, delta_order: int) -> HealpixPixel:
+        if self.order - delta_order < 0:
+            raise ValueError("Pixel Order cannot be below zero")
+        new_order = self.order - delta_order
+        new_pixel = math.floor(self.pixel / 4**delta_order)
+        return HealpixPixel(new_order, new_pixel)
+
+    def convert_to_higher_order(self, delta_order: int) -> List[HealpixPixel]:
+        if self.order + delta_order > HIPSCAT_ID_HEALPIX_ORDER:
+            raise ValueError(
+                f"Pixel Order cannot be above maximum order {HIPSCAT_ID_HEALPIX_ORDER}"
+            )
+        pixels = []
+        new_order = self.order + delta_order
+        for new_pixel in range(self.pixel * 4**delta_order, (self.pixel + 1) * 4**delta_order):
+            pixels.append(HealpixPixel(new_order, new_pixel))
+        return pixels
--- src/hipscat/pixel_tree/pixel_tree_builder.py
+++ src/hipscat/pixel_tree/pixel_tree_builder.py
@@ -5,11 +5,11 @@ from __future__ import annotations
 import pandas as pd
 
 from hipscat.catalog.partition_info import PartitionInfo
+from hipscat.pixel_math.healpix_pixel_convertor import (HealpixInputTypes,
+                                                        get_healpix_pixel)
 from hipscat.pixel_tree.pixel_node import PixelNode
 from hipscat.pixel_tree.pixel_node_type import PixelNodeType
 from hipscat.pixel_tree.pixel_tree import PixelTree
-from hipscat.pixel_math.healpix_pixel_convertor import (HealpixInputTypes,
-                                                        get_healpix_pixel)
 
 
 class PixelTreeBuilder:
@@ -80,7 +80,7 @@ class PixelTreeBuilder:
             return self.pixels[pixel.order][pixel.pixel]
         return None
 
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> PixelNode:
         return self.get_node(item)
 
     def _create_tree_from_partition_info_df(self, partition_info_df: pd.DataFrame):
@@ -141,7 +141,12 @@ class PixelTreeBuilder:
         self.create_node(pixel, node_type, parent)
 
     def create_node(
-        self, pixel: HealpixInputTypes, node_type: PixelNodeType, parent: PixelNode
+            self,
+            pixel: HealpixInputTypes,
+            node_type: PixelNodeType,
+            parent: PixelNode = None,
+            split_leaf_parent=False,
+            replace_existing_node=False,
     ):
         """Create a node and add to `self.pixels` in the tree
 
@@ -149,10 +154,92 @@ class PixelTreeBuilder:
             pixel: HEALPix pixel to get. Either of type `HealpixPixel`
                 or a tuple of (order, pixel)
             node_type: Node type of the node to create
-            parent: Parent node of the node to create
+            parent: (Default: None) Parent node of the node to create. If None, will get parent from
+                existing node in tree at correct position based on pixel
+            split_leaf_parent: (Default: False) If True and parent is of node type leaf, the parent
+                node in the tree will be changed to an inner node and leaf nodes for all child
+                pixels will be created.
+            replace_existing_node: (Default: False) If True and node at `pixel` already exists,
+                the existing node will be removed and replaced with the new node.
         """
         pixel = get_healpix_pixel(pixel)
+        if parent is None:
+            if pixel.order == 0:
+                parent = self.root_pixel
+            else:
+                parent_pixel = pixel.convert_to_lower_order(delta_order=1)
+                if parent_pixel not in self:
+                    raise ValueError(
+                        f"Cannot create node at {str(pixel)}, "
+                        f"no parent node exists at {str(parent_pixel)}"
+                    )
+                parent = self[parent_pixel]
+        if parent.node_type == PixelNodeType.LEAF:
+            if not split_leaf_parent:
+                raise ValueError(
+                    f"Cannot create node at {str(pixel)}, "
+                    f"parent node at {str(parent_pixel)} has node type leaf"
+                )
+            parent.node_type = PixelNodeType.INNER
+            for child_pixel in parent.pixel.convert_to_higher_order(delta_order=1):
+                if child_pixel != pixel:
+                    self.create_node(child_pixel, PixelNodeType.LEAF, parent=parent)
+        if pixel in self:
+            if not replace_existing_node:
+                raise ValueError(
+                    f"Cannot create node at {str(pixel)}, node already exists"
+                )
+            node_to_delete = self[pixel]
+            parent.remove_child_node(node_to_delete)
         node = PixelNode(pixel, node_type, parent)
         if pixel.order not in self.pixels:
             self.pixels[pixel.order] = {}
         self.pixels[pixel.order][pixel.pixel] = node
+
+    def remove_node(self, pixel: HealpixInputTypes):
+        pixel = get_healpix_pixel(pixel)
+        if pixel not in self:
+            raise ValueError(f"No node at pixel {str(pixel)}")
+        node = self[pixel]
+        node.parent.remove_child_node(node)
+        self.pixels[pixel.order].pop(pixel.pixel)
+        if len(self.pixels[pixel.order]) == 0:
+            self.pixels.pop(pixel.order)
+
+    def add_all_children_from_node(
+            self,
+            tree: PixelTree,
+            pixel: HealpixInputTypes,
+            split_leaf_parent=False,
+            replace_existing_node=False
+    ):
+        pixel = get_healpix_pixel(pixel)
+        nodes_to_add = tree[pixel].children
+        while len(nodes_to_add) > 0:
+            node = nodes_to_add.pop(0)
+            nodes_to_add += node.children
+            self.create_node(
+                node.pixel,
+                node.node_type,
+                split_leaf_parent=split_leaf_parent,
+                replace_existing_node=replace_existing_node
+            )
+
+    def remove_empty_inner_nodes(self):
+        nodes_to_check = [child for child in self.root_pixel.children]
+        for node in nodes_to_check:
+            self._remove_node_if_no_leaf_children(node)
+
+    def _remove_node_if_no_leaf_children(self, node: PixelNode) -> bool:
+        if node.node_type == PixelNodeType.LEAF:
+            return True
+        if len(node.children) == 0:
+            self.remove_node(node.pixel)
+            return False
+        has_leaf_children = False
+        nodes_to_check = [child for child in node.children]
+        for child in nodes_to_check:
+            has_leaf_children = has_leaf_children or self._remove_node_if_no_leaf_children(child)
+        if not has_leaf_children:
+            self.remove_node(node.pixel)
+        return has_leaf_children
