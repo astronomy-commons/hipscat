59b6fee9d459efee308512344d3b3a949a78109a
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-10-19 13:28:51 -0400

    local commit

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -37,6 +37,7 @@ def time_test_cone_filter_multiple_order():
     pixels = [HealpixPixel(6, 30), HealpixPixel(7, 124), HealpixPixel(8, 5000)]
     catalog = Catalog(catalog_info, pixels)
     filtered_catalog = catalog.filter_by_cone(47.1, 6, 30)
+    print("filtered_catalog", filtered_catalog.get_healpix_pixels())
     assert filtered_catalog.get_healpix_pixels() == [HealpixPixel(6, 30), HealpixPixel(7, 124)]
 
 
