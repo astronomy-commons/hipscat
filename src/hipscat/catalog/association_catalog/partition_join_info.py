1d1f5380c5d60a72422b1a184f05d369734c26c4
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-11-15 12:39:41 -0500

    Create join info from metadata file.

--- src/hipscat/catalog/association_catalog/partition_join_info.py
+++ src/hipscat/catalog/association_catalog/partition_join_info.py
@@ -1,9 +1,15 @@
-from typing import Any, Dict, Union
+"""Container class to hold primary-to-join partition metadata"""
 
 import pandas as pd
+import pyarrow as pa
 from typing_extensions import Self
 
 from hipscat.io import FilePointer, file_io
+from hipscat.io.parquet_metadata import (
+    read_row_group_fragments,
+    row_group_stat_single_value,
+    write_parquet_metadata_for_batches,
+)
 
 
 class PartitionJoinInfo:
@@ -15,7 +21,7 @@ class PartitionJoinInfo:
     JOIN_PIXEL_COLUMN_NAME = "join_Npix"
 
     COLUMN_NAMES = [
-        PRIMARY_PIXEL_COLUMN_NAME,
+        PRIMARY_ORDER_COLUMN_NAME,
         PRIMARY_PIXEL_COLUMN_NAME,
         JOIN_ORDER_COLUMN_NAME,
         JOIN_PIXEL_COLUMN_NAME,
@@ -30,10 +36,56 @@ class PartitionJoinInfo:
             if column not in self.data_frame.columns:
                 raise ValueError(f"join_info_df does not contain column {column}")
 
+    def write_to_metadata_files(self, catalog_path: FilePointer, storage_options: dict = None):
+        """Generate parquet metadata, using the known partitions.
+
+        Args:
+            catalog_path (str): base path for the catalog
+            storage_options: dictionary that contains abstract filesystem credentials
+        """
+        batches = [
+            pa.RecordBatch.from_arrays(
+                [
+                    [row[self.PRIMARY_ORDER_COLUMN_NAME]],
+                    [row[self.PRIMARY_PIXEL_COLUMN_NAME]],
+                    [row[self.JOIN_ORDER_COLUMN_NAME]],
+                    [row[self.JOIN_PIXEL_COLUMN_NAME]],
+                ],
+                names=self.COLUMN_NAMES,
+            )
+            for index, row in self.data_frame.iterrows()
+        ]
+
+        write_parquet_metadata_for_batches(batches, catalog_path, storage_options)
+
     @classmethod
-    def read_from_file(
-        cls, partition_join_info_file: FilePointer, storage_options: Union[Dict[Any, Any], None] = None
-    ) -> Self:
+    def read_from_file(cls, metadata_file: FilePointer, storage_options: dict = None):
+        """Read partition join info from a `_metadata` file to create an object
+
+        Args:
+            metadata_file: FilePointer to the `_metadata` file
+            storage_options: dictionary that contains abstract filesystem credentials
+
+        Returns:
+            A `PartitionJoinInfo` object with the data from the file
+        """
+        pixel_frame = pd.DataFrame(
+            [
+                (
+                    row_group_stat_single_value(row_group, cls.PRIMARY_ORDER_COLUMN_NAME),
+                    row_group_stat_single_value(row_group, cls.PRIMARY_PIXEL_COLUMN_NAME),
+                    row_group_stat_single_value(row_group, cls.JOIN_ORDER_COLUMN_NAME),
+                    row_group_stat_single_value(row_group, cls.JOIN_PIXEL_COLUMN_NAME),
+                )
+                for row_group in read_row_group_fragments(metadata_file, storage_options)
+            ],
+            columns=cls.COLUMN_NAMES,
+        )
+
+        return cls(pixel_frame)
+
+    @classmethod
+    def read_from_csv(cls, partition_join_info_file: FilePointer, storage_options: dict = None) -> Self:
         """Read partition join info from a `partition_join_info.csv` file to create an object
 
         Args:
