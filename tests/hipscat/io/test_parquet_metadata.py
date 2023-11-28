895061a9794aed06ef869e28f67632d2a03ec0a9
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-11-28 07:50:05 -0500

    More flexibility in parquet metadata methods.

--- tests/hipscat/io/test_parquet_metadata.py
+++ tests/hipscat/io/test_parquet_metadata.py
@@ -133,6 +133,14 @@ def test_row_group_fragments(small_sky_order1_dir):
     assert num_row_groups == 4
 
 
+def test_row_group_fragments_with_dir(small_sky_order1_dir):
+    num_row_groups = 0
+    for _ in read_row_group_fragments(small_sky_order1_dir):
+        num_row_groups += 1
+
+    assert num_row_groups == 4
+
+
 def test_row_group_stats(small_sky_dir):
     partition_info_file = paths.get_parquet_metadata_pointer(small_sky_dir)
     first_row_group = next(read_row_group_fragments(partition_info_file))
@@ -191,3 +199,25 @@ def test_get_healpix_pixel_from_metadata_fail(tmp_path):
     single_metadata = file_io.read_parquet_metadata(metadata_filename)
     with pytest.raises(ValueError, match="missing Norder"):
         get_healpix_pixel_from_metadata(single_metadata)
+
+
+def test_get_healpix_pixel_from_metadata_columns(tmp_path):
+    """Test fetching the healpix pixel from columns with non-default names."""
+    non_healpix_dataframe = pd.DataFrame({"data": [1], "Npix": [45], "join_Norder": [2], "join_Npix": [3]})
+    metadata_filename = os.path.join(tmp_path, "non_healpix_metadata.parquet")
+    non_healpix_dataframe.to_parquet(metadata_filename)
+    single_metadata = file_io.read_parquet_metadata(metadata_filename)
+    with pytest.raises(ValueError, match="missing Norder"):
+        get_healpix_pixel_from_metadata(single_metadata)
+
+    pixel = get_healpix_pixel_from_metadata(single_metadata, norder_column="data")
+    assert pixel == HealpixPixel(1, 45)
+
+    pixel = get_healpix_pixel_from_metadata(
+        single_metadata, norder_column="join_Norder", npix_column="join_Npix"
+    )
+    assert pixel == HealpixPixel(2, 3)
+
+    ## People can do silly things!
+    pixel = get_healpix_pixel_from_metadata(single_metadata, norder_column="data", npix_column="join_Npix")
+    assert pixel == HealpixPixel(1, 3)
