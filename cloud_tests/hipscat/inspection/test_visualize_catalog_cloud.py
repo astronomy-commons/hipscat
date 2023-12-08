9608adcac09db7ff7f3724ae34f86791fea48e7c
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-12-08 14:47:32 -0500

    Remove cloud_tests.

--- cloud_tests/hipscat/inspection/test_visualize_catalog_cloud.py
+++
@@ -1,9 +0,0 @@
-from hipscat.catalog import Catalog
-from hipscat.inspection import plot_pixels, plot_points
-
-
-def test_generate_map_order1(small_sky_dir_cloud, example_cloud_storage_options):
-    """Basic test that map data can be generated (does not test that a plot is rendered)"""
-    cat = Catalog.read_from_hipscat(small_sky_dir_cloud, storage_options=example_cloud_storage_options)
-    plot_pixels(cat, draw_map=False)
-    plot_points(cat, draw_map=False)
