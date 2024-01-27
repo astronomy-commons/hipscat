b6924769fb116777a9847907f26dc8199111fbff
Sean McGuire <seanmcgu@andrew.cmu.edu>
2024-01-26 16:36:54 -0800

    add margin catalog

--- src/hipscat/catalog/__init__.py
+++ src/hipscat/catalog/__init__.py
@@ -1,6 +1,7 @@
 """Catalog data wrappers"""
 
 from .association_catalog.association_catalog import AssociationCatalog
+from .margin_cache.margin_catalog import MarginCatalog
 from .catalog import Catalog
 from .catalog_type import CatalogType
 from .dataset.dataset import Dataset
