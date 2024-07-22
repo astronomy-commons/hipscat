9bac80916a716c82ff8c0fe03f6fdf085d494e30
Sandro Campos <scampos@andrew.cmu.edu>
2024-07-22 16:04:37 -0400

    Readd unit tests

--- tests/hipscat/pixel_math/test_partition_stats.py
+++ tests/hipscat/pixel_math/test_partition_stats.py
@@ -7,6 +7,7 @@ import pytest
 
 import hipscat.pixel_math as hist
 import hipscat.pixel_math.healpix_shim as hp
+from hipscat.pixel_math import HealpixPixel
 
 
 def test_small_sky_same_pixel():
@@ -196,3 +197,67 @@ def test_alignment_even_sky(drop_empty_siblings):
     # everything maps to order 7 (would be 5, but lowest of 7 is enforced)
     for mapping in result:
         assert mapping[0] == 7
+
+
+def test_compute_pixel_map_order1():
+    """Create destination pixel map for small sky at order 1"""
+
+    initial_histogram = hist.empty_histogram(1)
+    filled_pixels = [51, 29, 51, 0]
+    initial_histogram[44:] = filled_pixels[:]
+
+    expected = {HealpixPixel(0, 11): (131, [44, 45, 46])}
+
+    result = hist.compute_pixel_map(initial_histogram, highest_order=1, threshold=150)
+
+    npt.assert_array_equal(result, expected)
+
+    expected = {
+        HealpixPixel(1, 44): (51, [44]),
+        HealpixPixel(1, 45): (29, [45]),
+        HealpixPixel(1, 46): (51, [46]),
+    }
+
+    result = hist.compute_pixel_map(initial_histogram, highest_order=1, threshold=100)
+
+    npt.assert_array_equal(result, expected)
+
+
+@pytest.mark.timeout(5)
+def test_compute_pixel_map_even_sky():
+    """Create alignment from an even distribution at order 6"""
+    initial_histogram = np.full(hp.order2npix(6), 200)
+    result = hist.compute_pixel_map(initial_histogram, highest_order=6, threshold=1_000)
+    # everything maps to order 5, given the density
+    for mapping in result:
+        assert mapping.order == 5
+
+
+@pytest.mark.timeout(5)
+def test_compute_pixel_map_even_sky_enforce_lowest():
+    """Create pixel map for an even distribution, and enforce a lowest order bound."""
+    initial_histogram = np.full(hp.order2npix(6), 10)
+    result = hist.compute_pixel_map(initial_histogram, highest_order=6, lowest_order=4, threshold=1_000)
+    # everything maps to order 4 (would be 0, but lowest of 4 is enforced)
+    for mapping in result:
+        assert mapping.order == 4
+
+
+def test_compute_pixel_map_invalid_inputs():
+    """Create destination pixel map for small sky at order 1"""
+
+    initial_histogram = hist.empty_histogram(1)
+    filled_pixels = [51, 29, 51, 0]
+    initial_histogram[44:] = filled_pixels[:]
+
+    ## Order doesn't match histogram length
+    with pytest.raises(ValueError, match="histogram is not the right size"):
+        hist.compute_pixel_map(initial_histogram, highest_order=2, threshold=150)
+
+    ## Some bins exceed threshold
+    with pytest.raises(ValueError, match="exceeds threshold"):
+        hist.compute_pixel_map(initial_histogram, highest_order=1, threshold=30)
+
+    ## lowest_order too large
+    with pytest.raises(ValueError, match="lowest_order"):
+        hist.compute_pixel_map(initial_histogram, highest_order=1, lowest_order=2, threshold=30)
